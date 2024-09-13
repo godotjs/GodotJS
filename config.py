bdd1e406c94373f00e3ade250562c343730a9dd0
ialex32x <lin_032@hotmail.com>
2024-09-13 16:47:10 +0800

    feat: can build for android/ios

--- config.py
+++ config.py
@@ -3,8 +3,8 @@
 def can_build(env, platform):
     # temp
     # return platform == "windows"
-    return platform in ["windows", "macos", "linuxbsd"] # currently supported platforms
-    # return True
+    # return platform in ["windows", "macos", "linuxbsd", "android", "ios"] # currently supported platforms
+    return True
     
 def configure(env):
     pass
