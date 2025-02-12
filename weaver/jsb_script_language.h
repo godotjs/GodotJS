b0c3b3f8c4955062f6d15e2c6e70bfe77b457b9d
ialex32x <lin_032@hotmail.com>
2025-02-12 12:13:34 +0800

    fix: godot profiler crashs on GodotJSScriptLanguage::profiling_get_* (should return 0 for no data)

--- weaver/jsb_script_language.h
+++ weaver/jsb_script_language.h
@@ -145,8 +145,8 @@ public:
     {
     }
 
-    virtual int profiling_get_accumulated_data(ProfilingInfo* p_info_arr, int p_info_max) override { return -1; }
-    virtual int profiling_get_frame_data(ProfilingInfo* p_info_arr, int p_info_max) override { return -1; }
+    virtual int profiling_get_accumulated_data(ProfilingInfo* p_info_arr, int p_info_max) override { return 0; }
+    virtual int profiling_get_frame_data(ProfilingInfo* p_info_arr, int p_info_max) override { return 0; }
 
     virtual bool handles_global_class_type(const String& p_type) const override;
     virtual String get_global_class_name(const String& p_path, String* r_base_type = nullptr, String* r_icon_path = nullptr) const override;
