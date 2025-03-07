4f14f16b73d0a6f68229b3eb273dfd1cbe808965
Benjamin Dobell <git@benjamin.dobell.email>
2025-03-07 22:50:18 +1100

    feat: Statically resolved return type for get_node()

--- weaver-editor/jsb_editor_helper.h
+++ weaver-editor/jsb_editor_helper.h
@@ -6,12 +6,18 @@ class GodotJSEditorHelper : public Object
 {
     GDCLASS(GodotJSEditorHelper, Object);
 
+private:
+
+    static Dictionary _build_node_path_map(Node *node);
+    static void _log_scene_load_error(const String& p_file, Error p_error);
+
 protected:
     static void _bind_methods();
 
 public:
     virtual ~GodotJSEditorHelper() override = default;
 
+    static Dictionary get_scene_nodes(const String &p_path);
     static void show_toast(const String& p_text, int p_severity);
 };
 
