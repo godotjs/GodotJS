b3e615b8c131a626b49cd3d9a56f4ed0546e0133
Nicolas Merget <nicolas.merget@deutschebahn.com>
2025-12-12 13:34:55 +0100

    chore: add generated files

---
+++ tests/project/gen/godot/config/test.json.gen.ts
@@ -0,0 +1,5 @@
+declare module "godot" {
+    interface ResourceTypes {
+        "res://config/test.json": JSON;
+    }
+}
---
+++ tests/project/gen/godot/tests/papaparse/utils.ts.gen.ts
@@ -0,0 +1,5 @@
+declare module "godot" {
+    interface ResourceTypes {
+        "res://tests/papaparse/utils.ts": Script;
+    }
+}
