6721a89ebe9b00ae43161df1e1ada5b72006dca1
Nicolas Merget <nicolas.merget@deutschebahn.com>
2025-12-16 13:12:06 +0100

    fix: revert unused dot_slash implementation

--- bridge/jsb_module_resolver.cpp
+++ bridge/jsb_module_resolver.cpp
@@ -384,7 +384,7 @@ namespace jsb
 
                         // Transform main to equivalent exports
                         package_exports[dot] = main.begins_with(dot) ? main : internal::PathUtil::combine(dot, main);
-                        package_exports[dot_slash] = main.begins_with(dot_slash) ? main : internal::PathUtil::combine(dot_slash, main);
+                        package_exports[dot_slash] = dot_slash;
                     }
                 }
             }
